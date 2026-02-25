4cba4d1f0bc128bd9073b160fca28b27f1f6d6fa
Ashish Khurana <114246997+ashish2125it@users.noreply.github.com>
2026-02-25 16:41:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
@@ -1,42 +1,44 @@
 class Solution {
   public:
     bool isCyclic(int V, vector<vector<int>> &edges) {
-  vector<int>ans;
-    unordered_map<int,vector<int>>mp;
-    vector<int>indegree(V,0);
+   unordered_map<int,vector<int>>gr;
+    vector<int>degree(V,0);
     for(int i=0;i<edges.size();i++)
     {
-         int u = edges[i][0];
-        int v=  edges[i][1];
-        mp[u].push_back(v);
-        indegree[v]++;
+        int u = edges[i][0];
+        int v = edges[i][1];
+        degree[v]++;
+        gr[edges[i][0]].push_back(edges[i][1]);
     }
+    
     
     queue<int>q;
     for(int i=0;i<V;i++)
     {
-         if(indegree[i]==0)
-         q.push(i);
+        if(degree[i]==0)
+        q.push(i);
     }
     
+    vector<int>ans;
     while(q.size())
     {
         int size = q.size();
         for(int i=0;i<size;i++)
         {
             int node = q.front();
-            q.pop();
             ans.push_back(node);
-            for(auto Ne:mp[node])
+            q.pop();
+            for(auto ne:gr[node])
             {
-                indegree[Ne]--;
-                if(indegree[Ne]==0)
-                q.push(Ne);
+                degree[ne]--;
+                if(degree[ne]==0)
+                q.push(ne);
             }
         }
     }
     
     return ans.size()!=V;
+    
         
     }
 };
